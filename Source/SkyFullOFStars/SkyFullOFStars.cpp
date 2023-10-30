fa60404545e7c80d6ce1e6b141971bf813c6d05a
ritaparedes <ritacparedes@gmail.com>
2023-10-30 11:40:03 +0000

    Empty world

---
+++ Source/SkyFullOFStars/SkyFullOFStars.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkyFullOFStars.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkyFullOFStars, "SkyFullOFStars" );
---
+++ Source/SkyFullOFStars/SkyFullOFStarsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkyFullOFStarsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYFULLOFSTARS_API ASkyFullOFStarsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
